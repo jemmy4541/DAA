662e1cba8c6961d3a5a2e3077b47a651d2079f87
jemmy4541 <jemmy4541@gmail.com>
2022-11-17 00:16:59 +0530

    jj

--- adaa5_1.py
+++ adaa5_1.py
@@ -1,17 +1,21 @@
-def partition(array, low, high):
-	pivot = array[high]
-	i = low - 1
-	for j in range(low, high):
-		if array[j] <= pivot:
-			i = i + 1
-			(array[i], array[j]) = (array[j], array[i])
-	(array[i + 1], array[high]) = (array[high], array[i + 1])
-	return i + 1
-def quick_sort(array, low, high):
-	if low < high:
-		pi = partition(array, low, high)
-		quick_sort(array, low, pi - 1)
-		quick_sort(array, pi + 1, high)
-array = [10, 7, 8, 9, 1, 5]
-quick_sort(array, 0, len(array) - 1)
-print(f'Sorted array: {array}')
+def quicksort(arr, low, high):
+    if low<high:
+        pi=partition(arr, low, high)
+        quicksort(arr, low, pi-1)
+        quicksort(arr, pi+1, high)
+def partition(arr, low, high):
+    pivot=arr[high]
+    
+    i=low-1
+    for j in range(low, high):
+        if array[j]<=pivot:
+            i=i+1
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    (arr[i+1],arr[high])=(arr[high], arr[i+1])
+    return i+1
+        
+data = [1, 7, 4, 1, 10, 9, -2]
+size = len(data)
+quickSort(data, 0, size - 1)
+print('Sorted Array in Ascending Order:')
+print(data)
--- adaa5_2.py
+++ adaa5_2.py
@@ -1,32 +1,31 @@
-import random
-def quicksort(arr, start, stop):
-	if(start < stop):
-		pivotindex = partitionrand(arr,\
-							start, stop)
-		quicksort(arr , start , pivotindex)
-		quicksort(arr, pivotindex + 1, stop)
-def partitionrand(arr , start, stop):
-	randpivot = random.randrange(start, stop)
-	arr[start], arr[randpivot] =\
-		arr[randpivot], arr[start]
-	return partition(arr, start, stop)
-def partition(arr,start,stop):
-	pivot = start # pivot
-	i = start - 1
-	j = stop + 1
-	while True:
-		while True:
-			i = i + 1
-			if arr[i] >= arr[pivot]:
-				break
-		while True:
-			j = j - 1
-			if arr[j] <= arr[pivot]:
-				break
-		if i >= j:
-			return j
-		arr[i] , arr[j] = arr[j] , arr[i]
+import random 
+
+def quicksort(arr, low, high):
+    if low<high:
+        pi=pivotran(arr, low, high)
+        quicksort(arr, low, pi-1)
+        quicksort(arr, pi+1, high)
+        
+def pivotran(arr, low, high):
+    randpvt=random.randrange(low, high)
+    
+    arr[low], arr[randpvt]=arr[randpvt], arr[low]
+    
+    return partition(arr, low, high)
+
+def partition(arr, low, high):
+    pivot=low
+    
+    i=low+1
+    for j in range(low+1, high+1):
+        if arr[j]<=arr[pivot]:
+            arr[i],arr[j]=arr[j],arr[i]
+            i=i+1
+    arr[pivot], arr[i-1]=arr[i-1],arr[pivot]
+    pivot=i-1
+    return(pivot)
+
 if __name__ == "__main__":
-	array = [10, 7, 8, 9, 1, 5]
-	quicksort(array, 0, len(array) - 1)
-	print(array)
+    array = [10, 7, 8, 9, 1, 5]
+    quicksort(array, 0, len(array) - 1)
+    print(array)
