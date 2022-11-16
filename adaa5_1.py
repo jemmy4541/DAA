93d065a00b3270ab36b28e161a8fa2ef688dbd62
jemmy4541 <jemmy4541@gmail.com>
2022-11-16 21:16:24 +0530

    f

--- adaa5_1.py
+++ adaa5_1.py
@@ -1,7 +1,5 @@
 def partition(array, low, high):
-
 	pivot = array[high]
-
 	i = low - 1
 	for j in range(low, high):
 		if array[j] <= pivot:
@@ -9,15 +7,11 @@ def partition(array, low, high):
 			(array[i], array[j]) = (array[j], array[i])
 	(array[i + 1], array[high]) = (array[high], array[i + 1])
 	return i + 1
-def quickSort(array, low, high):
+def quick_sort(array, low, high):
 	if low < high:
 		pi = partition(array, low, high)
-		quickSort(array, low, pi - 1)
-		quickSort(array, pi + 1, high)
-data = [1, 7, 4, 1, 10, 9, -2]
-print("Unsorted Array")
-print(data)
-size = len(data)
-quickSort(data, 0, size - 1)
-pytprint('Sorted Array in Ascending Order:')
-print(data)
+		quick_sort(array, low, pi - 1)
+		quick_sort(array, pi + 1, high)
+array = [10, 7, 8, 9, 1, 5]
+quick_sort(array, 0, len(array) - 1)
+print(f'Sorted array: {array}')
